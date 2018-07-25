931b158b41f56a625e0a17ca140faad1f0a41003
akashofc2512 <akashofficial2512@gmail.com>
2018-07-25 17:40:23 +0530

    deploying log util lib

---
+++ logutils/src/main/java/com/akash/logutils/DoDebug.java
@@ -0,0 +1,10 @@
+package com.akash.logutils;
+
+import android.util.Log;
+
+public class DoDebug {
+
+    public static void D(String logKey,String message){
+        Log.d(logKey,message);
+    }
+}
