87015a80f6fa802ee866a16f5780af9e134e65c1
akashofc2512 <akashofficial2512@gmail.com>
2018-07-25 18:36:15 +0530

    added error log

--- app/src/main/java/com/akash/smaplelogutils/MainActivity.java
+++ app/src/main/java/com/akash/smaplelogutils/MainActivity.java
@@ -3,11 +3,15 @@ package com.akash.smaplelogutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.akash.logutils.DoDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DoDebug.D("myKey","Message: Hi Akash congrats");
+
     }
 }
--- logutils/src/main/java/com/akash/logutils/DoDebug.java
+++ logutils/src/main/java/com/akash/logutils/DoDebug.java
@@ -7,4 +7,7 @@ public class DoDebug {
     public static void D(String logKey,String message){
         Log.d(logKey,message);
     }
+    public static void E(String logKey,String message){
+        Log.e(logKey,message);
+    }
 }
